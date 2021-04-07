603177673e4c25ddb81e1186e5dcfb30b3e89384
Ulrik <ulrik.strid@outlook.com>
2021-04-07 21:12:59 +0200

    Update to latest sources

--- sources.nix
+++ sources.nix
@@ -4,8 +4,8 @@ args@{ overlays ? [ (import ./.) ], ... }:
 
 import
   (builtins.fetchTarball {
-    name = "nixos-unstable-2021-04-04";
-    url = https://github.com/nixos/nixpkgs/archive/8b65ff2559c1.tar.gz;
-    sha256 = "0pdha58pjc59dd6s0pf0y4rilzphxijik8zjvgjlb1q4yz5j0prr";
+    name = "nixos-unstable-2021-04-07";
+    url = https://github.com/nixos/nixpkgs/archive/de679c57ca57.tar.gz;
+    sha256 = "1vyxdgm39f4b3wsccqzwp4szj57f16nis1vz3mv8fxr1ycd2g2hj";
   })
   (args // { inherit overlays; })
